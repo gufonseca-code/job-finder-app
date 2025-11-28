8596f0eee5cee409ca1aed0209dff01d16269d85
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2025-11-28 13:21:59 -0300

    bugfix: remove redundant children rendering in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
-        {children}
       </body>
     </html>
   );

5c9f2859ea973354cfee0cdcde3ed1360c46ccc5
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2025-11-27 23:47:36 -0300

    refactor: improve layout and styling of the Home component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import InputKeywords from "@/components/InputKeywords"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold mb-6 text-gray-800">Bem-vindo ao Job Finder!</h1>
-      <p>Digite palavras chaves para começar a encontrar vagas de emprego</p>
+    <div className="min-h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="text-4xl font-bold mb-4 text-center text-gray-800">Bem-vindo ao Job Finder!</h1>
+      <p className="text-lg text-gray-600 mb-8 text-center">
+        Digite palavras chaves para começar a encontrar vagas de emprego
+      </p>
       <InputKeywords />
     </div>
   )
